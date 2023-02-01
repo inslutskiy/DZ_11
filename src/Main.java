0611c429bf4264181d853f8296db2e02dc9f598f
ilya <inslutskiy@gmail.com>
2023-02-01 18:01:14 +0300

    Add Longer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] a = {100, 5, 26, 58, 50};
+        long[] a = {100, 5, 26, 58, 50};
 
         SalesManager manager = new SalesManager(a);
 
